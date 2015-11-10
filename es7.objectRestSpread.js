eba37ec5684fd89e517304dc0f44c538d6821c5a
HE Shi-Jun <hax@heshijun.net>
2015-11-10 17:59:18 +0800

    remove tests for experimental features

--- es7.objectRestSpread.js
+++
@@ -1,6 +0,0 @@
-exports.test = function () {
-	var a = {x: 1, y: 2}, {x, ...a1, y, ...a2} = {x: 3, ...a, y: 4}
-	return x === 1 && y === 4
-		&& a1.x === undefined && a1.y === 4
-		&& a2.x === undefined && a2.y === undefined
-}
