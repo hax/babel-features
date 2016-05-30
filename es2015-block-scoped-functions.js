18e71260f9ec12406c574def64e17afd779fd8f4
HE Shi-Jun <hax@heshijun.net>
2016-05-31 05:43:09 +0800

    fix block-scoped-functions for non-strict mode

--- es2015-block-scoped-functions.js
+++ es2015-block-scoped-functions.js
@@ -1,9 +1,10 @@
+var outer = foo()
+function foo() { return 1 }
+
 {
 	var inner = foo()
 	function foo() { return 0 }
 }
-var outer = foo()
-function foo() { return 1 }
 
 assert(
 	inner === 0 && outer === 1
