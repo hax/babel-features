5829ac2348cd02d745256b1593babf198dda96e6
HE Shi-Jun <hax@heshijun.net>
2016-12-08 18:32:36 +0800

    add more block-scoping tests

--- features/es2015-block-scoping.js
+++ features/es2015-block-scoping.js
@@ -3,10 +3,14 @@ foo += bar
 {
 	const bar = 2
 	foo += bar
+	for (let bar = 3; bar < 5; foo += bar, ++bar) {
+		let bar = 10
+		foo += bar
+	}
 }
 
 assert(
-	foo === 3
+	foo === 30
 )
 
 switch (0) {
@@ -16,5 +20,17 @@ switch (0) {
 }
 
 assert(
-	foo === 6
+	foo === 33
+)
+
+const f = []
+for (let i = 0; i < 3; ++i) {
+	f.push(function () { return i })
+}
+
+assert(
+	f.length === 3
+	&& f[0]() === 0
+	&& f[1]() === 1
+	&& f[2]() === 2
 )
